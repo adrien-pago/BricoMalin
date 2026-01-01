d6cbb31ce1c008cff24dcef9de475fa6f6de388c
adrien-pago <adrien.pago@gmail.com>
2026-01-01 22:52:12 +0100

    gg

--- bricomalin_api/public/index.php
+++ bricomalin_api/public/index.php
@@ -2,6 +2,38 @@
 
 use App\Kernel;
 
+// Fix pour Apache qui ne transmet pas toujours le header Authorization à PHP
+// On récupère le header depuis différentes sources possibles et on le met dans $_SERVER
+if (!isset($_SERVER['HTTP_AUTHORIZATION'])) {
+    // Essayer depuis REDIRECT_HTTP_AUTHORIZATION (si mod_rewrite est utilisé)
+    if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+        $_SERVER['HTTP_AUTHORIZATION'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+    }
+    // Essayer depuis getallheaders() si disponible
+    elseif (function_exists('getallheaders')) {
+        $headers = getallheaders();
+        if ($headers) {
+            foreach ($headers as $name => $value) {
+                if (strtolower($name) === 'authorization') {
+                    $_SERVER['HTTP_AUTHORIZATION'] = $value;
+                    break;
+                }
+            }
+        }
+    }
+    // Essayer depuis apache_request_headers() si disponible
+    elseif (function_exists('apache_request_headers')) {
+        $headers = apache_request_headers();
+        if ($headers) {
+            if (isset($headers['Authorization'])) {
+                $_SERVER['HTTP_AUTHORIZATION'] = $headers['Authorization'];
+            } elseif (isset($headers['authorization'])) {
+                $_SERVER['HTTP_AUTHORIZATION'] = $headers['authorization'];
+            }
+        }
+    }
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
--- bricomalin_api/src/EventSubscriber/DebugHeadersSubscriber.php
+++ bricomalin_api/src/EventSubscriber/DebugHeadersSubscriber.php
@@ -32,34 +32,72 @@ class DebugHeadersSubscriber implements EventSubscriberInterface
         // On vérifie d'abord si le header existe dans la Request
         $authorization = $request->headers->get('Authorization');
         
+        // Log détaillé pour debug
+        error_log('=== DEBUG HEADERS ===');
+        error_log('Path: ' . $request->getPathInfo());
+        error_log('Method: ' . $request->getMethod());
+        error_log('Authorization from Request headers: ' . ($authorization ?: 'NOT FOUND'));
+        
         // Si le header n'existe pas, on essaie de le récupérer depuis $_SERVER
         if (!$authorization) {
+            error_log('Trying to get Authorization from $_SERVER...');
+            
+            // Log toutes les variables $_SERVER qui contiennent "AUTHORIZATION"
+            $authRelated = [];
+            foreach ($_SERVER as $key => $value) {
+                if (stripos($key, 'AUTHORIZATION') !== false || stripos($key, 'AUTH') !== false) {
+                    $authRelated[$key] = $value;
+                }
+            }
+            error_log('$_SERVER auth-related keys: ' . json_encode(array_keys($authRelated)));
+            
             // Apache peut stocker le header dans différentes variables $_SERVER
             if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
                 $authorization = $_SERVER['HTTP_AUTHORIZATION'];
                 $request->headers->set('Authorization', $authorization);
+                error_log('Found in $_SERVER[HTTP_AUTHORIZATION]');
             } elseif (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
                 $authorization = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
                 $request->headers->set('Authorization', $authorization);
+                error_log('Found in $_SERVER[REDIRECT_HTTP_AUTHORIZATION]');
             } elseif (function_exists('apache_request_headers')) {
+                error_log('Trying apache_request_headers()...');
                 // Dernière tentative avec apache_request_headers()
                 $apacheHeaders = apache_request_headers();
+                error_log('apache_request_headers() returned: ' . json_encode(array_keys($apacheHeaders ?? [])));
                 if (isset($apacheHeaders['Authorization'])) {
                     $authorization = $apacheHeaders['Authorization'];
                     $request->headers->set('Authorization', $authorization);
+                    error_log('Found in apache_request_headers()[Authorization]');
                 } elseif (isset($apacheHeaders['authorization'])) {
                     // Certaines versions d'Apache mettent en minuscules
                     $authorization = $apacheHeaders['authorization'];
                     $request->headers->set('Authorization', $authorization);
+                    error_log('Found in apache_request_headers()[authorization]');
+                }
+            }
+            
+            // Si toujours pas trouvé, lire directement depuis getallheaders() si disponible
+            if (!$authorization && function_exists('getallheaders')) {
+                error_log('Trying getallheaders()...');
+                $allHeaders = getallheaders();
+                if ($allHeaders) {
+                    error_log('getallheaders() returned: ' . json_encode(array_keys($allHeaders)));
+                    foreach ($allHeaders as $key => $value) {
+                        if (strtolower($key) === 'authorization') {
+                            $authorization = $value;
+                            $request->headers->set('Authorization', $authorization);
+                            error_log('Found in getallheaders()[' . $key . ']');
+                            break;
+                        }
+                    }
                 }
             }
         }
 
-        // Log des headers pour déboguer
-        error_log('=== DEBUG HEADERS ===');
-        error_log('Path: ' . $request->getPathInfo());
-        error_log('Authorization header: ' . ($authorization ?: 'NOT FOUND'));
-        error_log('All headers: ' . json_encode($request->headers->all()));
+        // Log final
+        error_log('Final Authorization header: ' . ($authorization ?: 'NOT FOUND'));
+        error_log('All Request headers: ' . json_encode($request->headers->all()));
         error_log('===================');
     }
 }
